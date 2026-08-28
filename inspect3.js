ffff577c4d22a728658c317baf27c984d56ab930
simon990520 <simon990520@github.com>
2026-08-28 13:03:31 -0500

    Limpiar scripts temporales de inspeccion

--- inspect3.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const file = 'D:/Desarrollos/falabella/home.html';
-const h = fs.readFileSync(file, 'utf8');
-
-// Ver el final del archivo para insertar el script antes de </body>
-console.log('largo total:', h.length);
-console.log('=== ULTIMOS 800 caracteres ===');
-console.log(h.substring(h.length - 800));
--- inspect4.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const h = fs.readFileSync('D:/Desarrollos/falabella/home.html', 'utf8');
-
-console.log('=== CONTEXTO 1er </body> ===');
-console.log(h.substring(1205400, 1212000));
